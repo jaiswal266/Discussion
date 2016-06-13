8d53e0317596d2e8a044745668d9d06cd5d80bb6
Akash Jaiswal <akash12jaiswalwork@gmail.com>
2016-06-13 14:05:14 +0530

    Commiting Basic Work

--- client/main.js
+++ client/main.js
@@ -2,5 +2,23 @@ import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
+CommentsList = new Mongo.Collection('comments');
+Meteor.subscribe('theComments');
 
-T
+Template.addCommentForm.events({
+	'submit form': function(event){
+		event.preventDefault();
+		var comment = event.target.commentBox.value;
+		var postedAt = new Date();
+		var postedBy = Meteor.user().emails[0].address;
+        Meteor.call('insertComment',postedAt,postedBy,comment);
+        event.target.commentBox.value = "";
+	}
+});
+Template.showComments.helpers({
+   getComments() {
+      return CommentsList.find({},{sort: {postedAt: -1}});
+	}
+});
+
+
--- server/main.js
+++ server/main.js
@@ -1,5 +1,18 @@
 import { Meteor } from 'meteor/meteor';
 
+CommentsList = new Mongo.Collection('comments');
+Meteor.publish('theComments',function(){
+   return CommentsList.find();
+});
+
+Meteor.methods({
+	'insertComment' : function(postedAt,postedBy,comment){
+		CommentsList.insert({
+			postedAt : postedAt, postedBy : postedBy, comment : comment
+		});
+	}
+});
+
 Meteor.startup(() => {
   // code to run on server at startup
 });
